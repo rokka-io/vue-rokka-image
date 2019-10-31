81dc49572c9697dd7a8525ffc2d041df10fb2b8d
Christian Stocker <me@chregu.tv>
2019-10-31 12:55:37 +0100

    export the right way

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import RokkaPicture from './Picture.vue'
 import RokkaSource from './Source.vue'
 import { rokkaUrl } from './helpers'
 
-module.exports = {
-  RokkaImg,
-  RokkaPicture,
-  RokkaSource,
-  rokkaUrl,
-  default: {
-    install(Vue) {
-      Vue.component('rokka-img', RokkaImg)
-      Vue.component('rokka-picture', RokkaPicture)
-      Vue.component('rokka-source', RokkaSource)
-    },
+export { RokkaImg, RokkaPicture, RokkaSource, rokkaUrl }
+export default {
+  install(Vue) {
+    Vue.component('rokka-img', RokkaImg)
+    Vue.component('rokka-picture', RokkaPicture)
+    Vue.component('rokka-source', RokkaSource)
   },
 }
