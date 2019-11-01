c74dd20e77313999bbee7e7cee1804b730753e6f
Christian Stocker <me@chregu.tv>
2019-11-01 15:03:43 +0100

    hopefully final rollup and dist config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,18 @@
 import vue from 'rollup-plugin-vue'
 import { terser } from 'rollup-plugin-terser'
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/index.iife.min.js',
-      format: 'iife',
+      file: 'dist/index.umd.min.js',
+      format: 'umd',
       name: 'vueRokkaImage',
       exports: 'named',
-      sourcemap: true
+      sourcemap: true,
     },
     { file: 'dist/index.esm.js', format: 'es' },
-    { file: 'dist/index.esm.min.js', format: 'es', sourcemap: true },
   ],
   plugins: [
     vue(),
